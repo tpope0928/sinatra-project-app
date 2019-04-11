f008612efca1997a6e8a55a4478811df3c32ca7c
Thomas Pope <tpope0928@gmail.com>
2019-04-11 00:04:13 +0000

    filled out get/post login with session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,22 @@ class UsersController < ApplicationController
     erb :login
   end
 
+  post '/login' do
+    @user = User.find_by(email: params[:email])
+    if @user.authenticate(params[:password])
+      session[:user_id] = @user.id #actually logging user in
+      puts session
+      redirect "/users/#{@user.id}"
+    else
+
+    end
+  end
+
   get '/signup' do
 
   end
 
-
+  get '/users/:id' do
+    "this will be the user show route"
+  end
 end
